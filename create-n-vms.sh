ab54a38b0c5fdac3371639e9de895b439829649d
Rafael Peria de Sene <rpsene@br.ibm.com>
2021-03-22 20:33:17 -0300

    Update the scripts to create VMs
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- create-n-vms.sh
+++ create-n-vms.sh
@@ -4,7 +4,7 @@ AMOUNT_VMS=$1
 IMAGE=$2
 NETWORK=$3
 
-for i in $(seq 1 $AMOUNT_VMS); do
+for i in $(seq 1 "$AMOUNT_VMS"); do
    SUFIX=$(openssl rand -hex 5)
-   ibmcloud pi instance-create $SUFIX --image $IMAGE --memory 4 --network $NETWORK --processors 1 --processor-type shared --key-name rpsene --sys-type s922
+   ibmcloud pi instance-create "$SUFIX$i" --image "$IMAGE" --memory 8 --network "$NETWORK" --processors 0.50 --processor-type shared --key-name rpsene --sys-type s922
 done
--- create-single-vm.sh
+++ create-single-vm.sh
@@ -13,7 +13,6 @@ function ctrl_c() {
     exit 1
 }
 
-VOLUME_ID=""
 SERVER_NAME=""
 
 function check_dependencies() {
@@ -22,7 +21,7 @@ function check_dependencies() {
     check_connectivity
     for i in "${DEPENDENCIES[@]}"
     do
-        if ! command -v $i &> /dev/null; then
+        if ! command -v "$i" &> /dev/null; then
             echo "$i could not be found, exiting!"
             exit 1
         fi
@@ -38,57 +37,57 @@ function check_connectivity() {
     fi
 }
 
-function create_nfs_server () {
+function create_server () {
 
-    SERVER_ID=$1
-    SERVER_IMAGE=$2
-    PUBLIC_NETWORK=$3
-    PRIVATE_NETWORK=$4
-    SSH_KEY_NAME=$5
+    local SERVER_ID=$1
+    local SERVER_IMAGE=$2
+    local PUBLIC_NETWORK=$3
+    local SSH_KEY_NAME=$4
 
     # Default values.
-    SERVER_MEMORY=$6
-    SERVER_PROCESSOR=$7
-    SERVER_SYS_TYPE=$8
+    local SERVER_MEMORY=$5
+    local SERVER_PROCESSOR=$6
+    local SERVER_SYS_TYPE=$7
 
-    ibmcloud pi instance-create $SERVER_NAME --image $SERVER_IMAGE --memory $SERVER_MEMORY --network $PUBLIC_NETWORK --processors $SERVER_PROCESSOR --processor-type shared --key-name $SSH_KEY_NAME --sys-type $SERVER_SYS_TYPE --json >> server.log
+    local SSH_USER=$8
 
-    NFS_SERVER_ID=$(cat ./server.log | jq -r ".[].pvmInstanceID")
-    NFS_SERVER_NAME=$(cat ./server.log | jq -r ".[].serverName")
+    ibmcloud pi instance-create "$SERVER_NAME" --image "$SERVER_IMAGE" --memory "$SERVER_MEMORY" --network "$PUBLIC_NETWORK" --processors "$SERVER_PROCESSOR" --processor-type shared --key-name "$SSH_KEY_NAME" --sys-type "$SERVER_SYS_TYPE" --json >> server.log | tee
 
-    echo "  $NFS_SERVER_NAME was created with the ID $NFS_SERVER_ID"
+    SERVER_ID=$(jq -r ".[].pvmInstanceID" < ./server.log)
+    SERVER_NAME=$(jq -r ".[].serverName" < ./server.log)
 
-    echo "NFS_SERVER_ID=$NFS_SERVER_ID" >> ./server-build.log
-    echo "NFS_SERVER_NAME=$NFS_SERVER_NAME" >> ./server-build.log
+    echo "  $SERVER_NAME was created with the ID $SERVER_ID"
 
-    echo "  deploying the server $NFS_SERVER_NAME, hold on please."
-    STATUS=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r ".status")
+    echo "SERVER_ID=$SERVER_ID" >> ./server-build.log
+    echo "SERVER_NAME=$SERVER_NAME" >> ./server-build.log
+
+    echo "  deploying the server $SERVER_NAME, hold on please."
+    STATUS=$(ibmcloud pi in "$SERVER_ID" --json | jq -r ".status")
 
     printf "%c" "    "
     while [[ "$STATUS" != "ACTIVE" ]]
     do
         sleep 5s
-        STATUS=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r ".status")
+        STATUS=$(ibmcloud pi in "$SERVER_ID" --json | jq -r ".status")
         printf "%c" "."
     done
 
     if [[ "$STATUS" == "ERROR" ]]; then
         echo "ERROR: a new VM could not be created, destroy the allocated resources..."
-        ibmcloud pi instance-delete $NFS_SERVER_ID
-        ibmcloud pi volume-delete $VOLUME_ID
+        ibmcloud pi instance-delete "$SERVER_ID"
     fi
 
     if [[ "$STATUS" == "ACTIVE" ]]; then
         echo
-        echo "  $NFS_SERVER_NAME is now ACTIVE."
+        echo "  $SERVER_NAME is now ACTIVE."
         echo "  waiting for the network availability, hold on please."
 
-        EXTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].externalIP')
+        EXTERNAL_IP=$(ibmcloud pi in "$SERVER_ID" --json | jq -r '.addresses[0].externalIP')
         printf "%c" "    "
         while [[ -z "$EXTERNAL_IP" ]]; do
             printf "%c" "."
-            EXTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].externalIP')
-            INTERNAL_IP=$(ibmcloud pi in $NFS_SERVER_ID --json | jq -r '.addresses[0].ip')
+            EXTERNAL_IP=$(ibmcloud pi in "$SERVER_ID" --json | jq -r '.addresses[0].externalIP')
+            INTERNAL_IP=$(ibmcloud pi in "$SERVER_ID" --json | jq -r '.addresses[0].ip')
             sleep 3s
         done
 
@@ -96,42 +95,45 @@ function create_nfs_server () {
         echo "SERVER_INTERNAL_IP=$INTERNAL_IP" >> ./server-build.log
     fi
     printf "%c" "    "
-    while ! ping -c 1 $EXTERNAL_IP &> /dev/null
+    while ! ping -c 1 "$EXTERNAL_IP" &> /dev/null
     do
-        sleep 3s
+        sleep 1
         printf "%c" "."
     done
+    until ssh -oStrictHostKeyChecking=no "$SSH_USER"@"$EXTERNAL_IP" 'uname -a; exit'; do
+        sleep 1
+    done
     echo
-    echo "  $NFS_SERVER_NAME is ready, access it using ssh root@$EXTERNAL_IP."
+    echo "  $SERVER_NAME is ready, access it using ssh at $EXTERNAL_IP."
 }
 
 function run (){
 
     echo "*****************************************************"
     SERVER_ID=$(openssl rand -hex 5)
-    SERVER_NAME="powervs-vm-$SERVER_ID"
+    SERVER_NAME="rpsene-$SERVER_ID"
 
     mkdir -p ./servers/"$SERVER_NAME"
-    cd ./servers/$SERVER_NAME
+    cd ./servers/"$SERVER_NAME" || exit 1
 
     ### Set this variables accordingly
     SERVER_IMAGE=
     PUBLIC_NETWORK=
-    PRIVATE_NETWORK=
     SSH_KEY_NAME=
-    SERVER_MEMORY=4
-    SERVER_PROCESSOR=1
+    SERVER_MEMORY=
+    SERVER_PROCESSOR=
     SERVER_SYS_TYPE=s922
+    SSH_USER=
     ####
 
     check_dependencies
     check_connectivity
-    start=`date +%s`
-    create_nfs_server $SERVER_ID $SERVER_IMAGE $PUBLIC_NETWORK $PRIVATE_NETWORK $SSH_KEY_NAME $SERVER_MEMORY $SERVER_PROCESSOR $SERVER_SYS_TYPE
-    end=`date +%s`
+    start=$(date +%s)
+    create_server "$SERVER_ID" "$SERVER_IMAGE" "$PUBLIC_NETWORK" "$SSH_KEY_NAME" "$SERVER_MEMORY" "$SERVER_PROCESSOR" "$SERVER_SYS_TYPE" "$SSH_USER"
+    end=$(date +%s)
     echo "*****************************************************"
     runtime=$((end-start))
-    echo $runtime
+    echo "TOTAL TIME: $runtime seconds" 
 }
 
 ### Main Execution ###
